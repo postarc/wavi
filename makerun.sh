c063b1f9f4f264ae5d8ab6ba90f984a8365b58e2
postarc <45208608+postarc@users.noreply.github.com>
2018-11-25 22:34:20 +0300

    Create makerun.sh

---
+++ makerun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# makerun.sh
+# Make sure geekcashd is always running.
+# Add the following to the crontab (i.e. crontab -e)
+# */1 * * * * ~/wavi/makerun.sh
+
+process=wavid
+makerun="wavid"
+
+if ps -u $USER | grep -v grep | grep $process > /dev/null
+then
+  exit
+else
+  $makerun -daemon $1 $2 &
+fi 
